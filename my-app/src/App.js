c76d0b8a341165d420cde6dd6f9e61a97dd245fc
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-04-21 16:38:02 +0700

    header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-//import "assets/scss/style.scss";
 import "assets/scss/style.scss";
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import LandingPage from "pages/LandingPage";
+
+
+
 
 function App() {
-  return <div className="App"> hallo world</div>;
+  return <div className="App">
+    <Router>
+      <Route path="/" component={LandingPage}>
+
+      </Route>
+    </Router>
+  
+  </div>;
 }
 
 export default App;
---
+++ my-app/src/parts/IconText.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//butuon di elemen
+import Button from 'elements/Button'
+
+export default function IconText() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+            Prei<span className='text-gray-900'>Lur</span><span className='brand-text-icon'>!</span>
+
+        </Button>
+    )
+}
